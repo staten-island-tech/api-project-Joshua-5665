139b57ca9015450624488bce9f702886f331e34c
Joshua-5665 <joshuac726@nycstudents.net>
2026-01-13 14:26:20 -0500

    h

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,56 +1,6 @@
 import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
 
-/* const api = "https://currencybeacon.com/";
-
-const jit = document.querySelector('.apibutton');
-jit.addEventListener('click', ()=> {
-  window.alert('Copy and Paste this Link:     ' + api);
-}) */
-
-
-const apikey = "7C8DezrVggCwVnkHt7BRyZKyIM8eUgKy";
-async function getData() {
-  try {
-    const response = await fetch(`/api/v1/latest?api_key=${apikey}`);
-    if (response.status != 200) {
-      throw new Error(response);
-    } else {
-      const data = await response.json();
-      console.log(data);
-    }
-  } catch(error) {
-    console.log(error);
-  }
-}
-getData();
-
-function moreOption(data) {
-  const breh1 = document.getElementById('breh1');
-  const breh2 = document.getElementById('breh2');
-  Object.entries(data.rates).forEach(([name, rate]) => {
-    const option1 = document.createElement(`option`);
-    option1.text = name;
-    breh1.add(option1);
-    const option2 = document.createElement(`option`);
-    option2.text = name;
-    breh2.add(option2);
-
-  });
-}
-convertCurrency();
-async function main() {
-  const data = await getData();
-  moreOption(data);
-}
-main();
-
-
-
-const click = document.getElementById('jit');
-const idk = document.getElementById('why')
-click.addEventListener('click', ()=> {
-  
-  idk.value = 'Conversion Error'
-
-})

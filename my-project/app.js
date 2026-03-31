ae01516537aba681ea61db39a45290dd5e668a34
Joshua-5665 <joshuac726@nycstudents.net>
2026-03-31 14:02:16 -0400

    ghr

--- my-project/app.js
+++ my-project/app.js
@@ -1,4 +1,4 @@
-const sushiOrders = [
+/* const sushiOrders = [
   { name: "California Roll", price: 8 },
   { name: "Spicy Tuna Roll", price: 10 },
   { name: "Salmon Nigiri", price: 6 },
@@ -17,7 +17,7 @@ function getReceipt(sushiOrders) {
         if(!receipt[item.name]) {
             receipt[item.name] = {
                 price: item.price,
-                qyt: 1,
+                qty: 1,
             };
         } else {
             receipt[item.name].qty++;
@@ -26,4 +26,46 @@ function getReceipt(sushiOrders) {
     console.log(receipt);
 }
 getReceipt(sushiOrders);
+
+function generateReceipt(orders) {
+    const itemMap = {}
+
+    orders.forEach((order) => {
+        if(itemMap[order.name]) {
+            itemMap[order.name].qty += 1;
+        } else {
+            itemMap[order.name] = {qty: 1, price: order.price};
+        }
+    })
+    let total = 0;
+    let receipt = "";
+
+    for (const [name, {qty, price }] of Object.entries(itemMap)) {
+        console.log(name, qty, price);
+
+        total += qty * price;
+        receipt += `${name} x${qty} $${price*qty}\n`
+    }
+    receipt += `TOTAL ${total}`
+    console.log(receipt);
+}
+generateReceipt(sushiOrders); */
+
+const department = {
+    Cardiology: ["Alice", "Bob", "Carol"],
+    Neurology: ["Diana", "Eve"],
+    Orthopedics: ["Frank", "Grace", "Hank"],
+    Oncology: ["Ivy", "Bob"]
+};
+
+const staff = {};
+Object.entries(department).forEach(([dept, doctors]) => {
+    console.log(dept, doctors);
+    if(!staff[doctors]) {
+        staff[doctors] = [dept];
+    } else {
+        staff[doctors].push(dept);
+    }
+    console.log(staff);
+})
 
